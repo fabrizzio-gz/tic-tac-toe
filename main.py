9af8bb91e0a77245df5c5c0fd1f5df85598bd80a
fabrizzio-gz <fabrizzio.gz@gmail.com>
2020-08-07 18:32:52 -0400

    Fixing code. Adding comments and docstrings

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-# tic-tac-toe
-import numpy as np
 from itertools import cycle
 from random import shuffle
+import numpy as np
+
+### TODO:
+# - 2 player mode.
+# - Choose player/cpu token.
+
 
 class NoMove(Exception):
     pass
+
 
 class Board:
 
@@ -58,38 +63,25 @@ class Board:
                 print('Horizontal check')
                 return True
 
-            # for j in range(3):
-            #     if not self.rows[i][j] == player:
-            #         break
-            # else:
-            #     print('Horizontal check')
-            #     return True
-
         # Vertical win
         for j in range(3):
             if np.sum(self.columns[j] == player) == 3:
                 print('Vertical check')
                 return True
-            # for i in range(3):
-            #     if not self.columns[j][i] == player:
-            #         break
-            # else:
-            #     print('Vertical check')
-            #     return True
 
         # Diagonal win
         for d in range(2):
             if np.sum(self.diags[d] == player) == 3:
                 print('Diagonal check')
                 return True
-        # if (board[0][0], board[1][1], board[2][2]) == (player, player, player) or \
-        #    (board[2][0], board[1][1], board[0][2]) == (player, player, player):
-        #     print('Diagonal Check')
-        #     return True
 
         return False
 
     def fill_next(self):
+        """
+        Scans all cases of the board from left to right, top to bottom.
+        Fills first empty case found with self.player token.
+        """
         for i in range(3):
             for j in range(3):
                 if not self.board[i][j]:
@@ -98,25 +90,24 @@ class Board:
 
     def fill_corner(self):
         """
-        Chooses one corner randomly, if it's empty, fills it 
-        with self.player token.
-        Raises NoMove exception if not possible for all corner. 
+        Chooses one corner randomly.
+        If it's empty, fills it with self.player token.
+        Raises NoMove exception if not possible for all corners.
         """
-        board = self.board
         random_corners = list(range(4))
         shuffle(random_corners)
         for i in random_corners:
-            if i == 0 and not board[0][0]:
-                board[0][0] = self.player
+            if i == 0 and not self.board[0][0]:
+                self.board[0][0] = self.player
                 break
-            elif i == 1 and not board[0][2]:
-                board[0][2] = self.player
+            elif i == 1 and not self.board[0][2]:
+                self.board[0][2] = self.player
                 break
-            elif i == 2 and not board[2][0]:
-                board[2][0] = self.player
+            elif i == 2 and not self.board[2][0]:
+                self.board[2][0] = self.player
                 break
-            elif i == 3 and not board[2][2]:
-                board[2][2] = self.player
+            elif i == 3 and not self.board[2][2]:
+                self.board[2][2] = self.player
                 break
         else:
             raise NoMove
@@ -124,122 +115,97 @@ class Board:
     def diagonal_free(self):
         """
         Check if center case is free.
-        Then checks the top left and right corners and tries
-        to fill it's opposed diagonal.
+        Then checks every corner in the board and fills its diagonally
+        opposed case if it's empty.
+        Raises NoMove exception if not possible for all corners.
         """
-        board = self.board
-        if not board[1][1]:
-            if board[0][0] == self.player and not board[2][2]:
-                board[2][2] = self.player
-            elif board[0][2] == self.player and not board[2][0]:
-                board[2][0] = self.player
+        if not self.board[1][1]:
+            for d in range(2):
+                for filled, empty in [(0, 2), (2, 0)]:
+                    if self.diags[d][filled] == self.player and \
+                       not self.diags[d][empty]:
+                        self.diags[d][empty] = self.player
+                        return None
             else:
                 raise NoMove
         else:
             raise NoMove
 
-
-    def get_diagonals(self):
-        return self.board.diagonal().copy(), np.diagonal(np.fliplr(self.board)).copy()
-    
-    
-    def complete_triple(self, target = None):
+    def complete_triple(self, target=None):###
         """
-        Will scan rows, columns and diagonals for 2 occurrences of
-        'target'. If 2 cases are filled with 'target', it will
-        fill the remaining case with self.player symbol.
-        If no 'target' is specified, it will use current player.symbol one.
+        target: A player token. If none is given, will use current
+        self.player token.
+        Scans rows, columns and diagonals for 2 occurrences of target token.
+        If 2 cases are filled with 'target', it will fill the remaining case
+        with target.
         If no occurrences take place, raises NoMove exception.
         """
+        def try_fill(array3):
+            """
+            array3: An array with 3 elements.
+            If two elements of the array are filled with 'target' token and
+            one is empty, fills the empty case with current self.player token.
+            Returns True if successful. False if not.
+            """
+            if np.sum(array3 == target) == 2 and \
+               np.sum(array3 == '') == 1:
+                array3[array3 == ''] = self.player
+                return True
+            return False
+            
         if not target:
             target = self.player
-            
-        board = self.board
-
-        def try_fill(array3, i):
-            if np.sum(array3[i] == target) == 2 and \
-               np.sum(array3[i] == '') == 1:
-                array3[i][array3[i] == ''] = self.player
-                return True
         
         # Rows
-        for i in range(3):
-            if try_fill(self.rows, i):
+        for row in self.rows:
+            if try_fill(row):
                 return None
-            # if np.sum(self.rows[i] == target) == 2 and \
-            #    np.sum(self.rows[i] == '') == 1:
-            #     self.rows[i][self.rows[i] == ''] = self.player
-            #     return None
 
         # Columns
-        for j in range(3):
-            if try_fill(self.columns, j):
+        for column in self.columns:
+            if try_fill(column):
                 return None
-            # if np.sum(self.columns[j] == target) == 2 and \
-            #    np.sum(self.columns[j] == '') == 1:
-            #     self.columns[j][self.columns[j] == ''] = self.player
-            #     return None
 
         # Diagonals
         for diagonal in self.diags:
-            if np.sum(diagonal == target) == 2 and \
-               np.sum(diagonal == '') == 1:
-                diagonal[diagonal == ''] = self.player
+            if try_fill(diagonal):
                 return None
-            
-        # diagonal0, diagonal1 = self.get_diagonals()
-        # if np.sum(diagonal0 == target) == 2 and \
-        #    np.sum(diagonal0 == '') == 1:
-        #     diagonal0[diagonal0 == ''] = self.player
-        #     self.board[np.diag_indices_from(self.board)] = diagonal0
-        #     return None
-        # if np.sum(diagonal1 == target) == 2 and \
-        #    np.sum(diagonal0 == '') == 1:
-        #     diagonal1[diagonal1 == ''] = self.player
-        #     # Inverted diagonal indices
-        #     dinv = (np.array([0, 1, 2]), np.array([2, 1, 0]))
-        #     self.board[dinv] = diagonal1
-        #     return None
-        
+
         raise NoMove
-    
+
     def win_move(self):
         """
-        Searches for rows, columns and diagonals with 2 occurrences of 
+        Searches for rows, columns and diagonals with 2 occurrences of
         self.symbol and completes it to win.
-        If no occurrences take place, raises NoMove exception.
+        If no occurrences take place, complete_triple raises NoMove exception.
         """
-        print('Trying to win...')
         self.complete_triple()
-        print('Won')
-    
+        return None
 
     def avoid_losing(self):
         """
-        Searches for rows, columns and diagonals with 2 occurrences of 
-        the opposed of self.symbol. Completes it to avoid losing.
+        Searches for rows, columns and diagonals with 2 occurrences of
+        the adversary of self.player token. 
+        Completes the empty case to avoid losing.
         If no occurrences take place, raises NoMove exception.
         """
         if self.player == 'x':
             target = 'o'
         else:
             target = 'x'
-        print('Avoiding losing...')
         self.complete_triple(target)
-        print('Achieved')
+        return None
 
-
-    def count_corners(self, target = None):
+    def count_corners(self, target=None):
         """
         Returns the number of corners occupied by the current
         self.player token. (0 - 4)
         """
         if not target:
             target = self.player
-            
-        return np.sum(self.board[[0,0,-1,-1],[0,-1,0,-1]] == target)
 
-    
+        return np.sum(self.board[[0, 0, -1, -1], [0, -1, 0, -1]] == target)
+
     def defend_corner(self):
         """
         Checks if the other player has filled one of the corners.
@@ -253,7 +219,6 @@ class Board:
 
         return self.count_corners(other_player) >= 1
 
-
     def scan_one(self, array3):
         """
         array3: An array with 3 elements.
@@ -262,8 +227,7 @@ class Board:
         Returns True if valid. If not, False.
         """
         return np.sum(array3 == self.player) == 1 and \
-               np.sum(array3 == '') == 2
-
+            np.sum(array3 == '') == 2
 
     def fill_one(self, array3):
         """
@@ -275,8 +239,9 @@ class Board:
             if not array3[i]:
                 array3[i] = self.player
                 return None
+
         raise NoMove
-    
+
     def complete_second(self):
         """
         Will scan rows, columns and diagonals (in that order)
@@ -298,19 +263,28 @@ class Board:
             if self.scan_one(diagonal):
                 self.fill_one(diagonal)
                 return None
-                
+
         raise NoMove
-        
-        
+
     def computer_turn(self):
+        """
+        Attempts different moves.
+        If any move succeds, it returns None.
+        When a move fails, it raises a NoMove exception and
+        attempts the next move.
+        """
+        self.turns +=1
         if self.player == 'x':
             other_player = 'o'
         else:
             other_player = 'x'
-        
-        self.turns += 1
+
         input('Computer turn... (Press any key to continue)')
+
+        # Starts by filling any of the corners.
         if self.turns <= 2:
+            # If the other player started the game and filled
+            # any of the corners, fill the center case.
             if self.turns == 2:
                 if self.defend_corner():
                     self.board[1][1] = self.player
@@ -321,45 +295,57 @@ class Board:
             except NoMove:
                 pass
 
+        # Attempts to win the game with a single move.
         try:
             self.win_move()
             return None
         except NoMove:
             pass
 
+        # If the opponent is about to complete a triple, avoid it.
         try:
             self.avoid_losing()
             return None
         except NoMove:
             pass
 
+        # Attempts filling the diagonally opposed case if
+        # the central case is empty.
         try:
             self.diagonal_free()
             return None
         except NoMove:
             pass
 
+        # During mid game, attempts to continue filling corners.
+        # First verifies that some corners are already filled by the current
+        # token or that the opponent hasn't filled the corners already so that
+        # it's a worthy move.
         if (self.turns <= 4 and self.count_corners(other_player) != 2) or \
            self.count_corners() >= 2:
-            ### STILL need to check for unimportant cases
             try:
                 self.fill_corner()
                 return None
             except NoMove:
                 pass
 
+        # If no other option, will complete any row/column/diagonal
+        # that has one player token and two empty cases.
         try:
             self.complete_second()
             return None
         except NoMove:
             pass
         
-        # If no other possible options
+        # If no other possible options, fill any empty case.
         self.fill_next()
         return None
-       
 
     def get_index(self, text):
+        """
+        Will prompt the player for a row or coluumn index.
+        Returns an integer index between 0 - 2.
+        """
         index = 0
         while True:
             index = input("Choose a {}\n".format(text))
@@ -374,15 +360,21 @@ class Board:
                 break
         return index
 
-
     def try_case(self, i, j):
+        """
+        Will atempt filling the case[i][j].
+        Returns True on success. False otherwise.
+        """
         if not self.board[i][j]:
             self.board[i][j] = self.player
             return True
-        else:
-            return False
-    
+        return False
+
     def user_turn(self):
+        """
+        Prompts the user for a row and a column index.
+        Verifies case isn't filled.
+        """
         self.turns += 1
         print('Your turn')
         while True:
@@ -394,8 +386,15 @@ class Board:
                 print('Case is filled. Choose another one!')
         return None
 
-    
     def play(self):
+        """
+        Game loop.
+        Starts with computer turn.
+        Alternates turns between player/computer.
+        Verifies if there is a winner.
+        Verifies if there is a draw.
+        Starts new game alternating from last player.
+        """
         current_player = self.player
         print('Welcome!')
         self.print_score()
@@ -431,56 +430,60 @@ class Board:
             else:
                 self.player = next(Board.players)
 
-                
     def endgame(self):
+        """
+        Checks if 9 turns of the game have elapsed.
+        """
         return self.turns == 9
 
-
     def reset(self):
+        """
+        Empties all board cases and sets turns to 0.
+        """
         self.board.fill('')
         self.turns = 0
 
-        
     def computer_win(self):
+        """
+        Prints computer winning message.
+        Increments computer score by one.
+        """
         print('Computer won!')
         input('Press enter to continue...')
         self.computer_score += 1
 
-        
     def player_win(self):
+        """
+        Prints player winning message.
+        Increases player score.
+        """
         print('You win!')
         input('Press enter to continue...')
         self.user_score += 1
 
-        
     def print_score(self):
-        print('Current score is:\n Player\t: {}\n CPU\t: {}'.format(self.user_score,
-               self.computer_score))
-    
+        """
+        Prints score
+        """
+        print('Current score is:\n Player\t: {}\n CPU\t: {}'
+              .format(self.user_score,self.computer_score))
+
     def __str__(self):
+        """
+        Game board pretty printing.
+        """
         dash = '---'
         counter = 0
         board_str = ''
         for row in self.board:
-            board_str += '{: ^5s}|{: ^5s}|{: ^5s}\n'.format(row[0], row[1], row[2])
+            board_str += '{: ^5s}|{: ^5s}|{: ^5s}\n'\
+                          .format(row[0], row[1], row[2])
             if counter < 2:
                 counter += 1
-                board_str += '{: ^5s}+{: ^5s}+{: ^5s}\n'.format(dash, dash, dash)
+                board_str += '{0: ^5s}+{0: ^5s}+{0: ^5s}\n'.format(dash)
+
         return board_str
+
 
 board = Board()
 board.play()
-a = np.ones(shape=(2,2))
-
-def f(array_, index):
-    array_[index][0] = 2
-    
-# for i in range(3):
-#     for j in range(3):
-#         board.set_case(i, j, 'x') #if (i + j) % 2 == 0 else 'o')
-#         board.check_winner()
-#         #board.reset()
-#         if board.endgame():
-#             print('The end!')
-#         else:
-#             print('Still going')
